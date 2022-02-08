d8adeed354e3ee4ee4052a1bb508210c24d97c08
Emily Harnish <72752507+slipperscocoa@users.noreply.github.com>
2022-02-08 16:10:54 -0500

    added Google Analytics to site

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import './App.css';
 // import LoginApp from './Login/LoginApp'; 
 // import HomePageApp from './HomePage/HomePage';
 import NavbarApp from './HomePage/Navbar.js'; 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-    // const [ token, setToken ] = useState(); 
+import ReactGA from 'react-ga';
 
-    // if(!token) {
-    //     return <LoginApp setToken={setToken} />
-    // }
+// function App() {
+//     // const [ token, setToken ] = useState(); 
 
+//     // if(!token) {
+//     //     return <LoginApp setToken={setToken} />
+//     // }
+
+//     setGA = () => {
+//       ReactGA.initialize('UA-207178733-1');
+//       ReactGA.pageview('Init page view');
+//     };
+//     componentDidMount = () => {
+//       this.setGA();
+//     };
+
+//     return (
+//     <div className="App">
+//       <div>
+//         <BrowserRouter>
+//             <Switch>
+//                 <Route exact path="/">
+//                     {/* <HomePageApp /> */}
+//                     <NavbarApp />
+//                 </Route>
+//             </Switch>
+//         </BrowserRouter>
+//       </div>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  setGA = () => {
+    ReactGA.initialize('UA-207178733-1');
+    ReactGA.pageview('Init page view');
+  };
+  componentDidMount = () => {
+    this.setGA();
+  };
+
+  render() {
     return (
-    <div className="App">
-      <div>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/">
-                    {/* <HomePageApp /> */}
-                    <NavbarApp />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+      <div className="App">
+        <div>
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path="/">
+                      {/* <HomePageApp /> */}
+                      <NavbarApp />
+                  </Route>
+              </Switch>
+          </BrowserRouter>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
